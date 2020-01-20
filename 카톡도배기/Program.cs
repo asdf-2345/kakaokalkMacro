94f155bd6f242c25294d3fd2527a55636ab1a690
asdf-2345 <52937308+asdf-2345@users.noreply.github.com>
2020-01-20 11:33:52 +0900

    카톡창을 찾는 방식을 변경하였습니다.
    원래 채팅방 이름을 입력하여 채팅방을 찾는 것 이었지만 이제 맨 앞에 있는 카톡채팅방을 찾아서 도배합니다.

--- 카톡도배기/Program.cs
+++ 카톡도배기/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
 using System.Threading;
@@ -21,23 +21,13 @@ namespace 카톡1
 		public static void Main()
 		{
 		first: Console.Clear();
-		string answer = "";
+		Console.WriteLine("※맨앞에 있는 카톡창에 보내집니다 주의하세요");
+			string answer = "";
 			Console.WriteLine("도배하시려는 말을 입력해주세요");
 			string str = Console.ReadLine();
-			Console.WriteLine("얼마나 반복하실겁니까?");
-			int number = 0;
-		a:
-			try{
-				number = int.Parse(Console.ReadLine());
-			}
-			catch{
-				Console.WriteLine("다시 입력해주세요.");
-				goto a;
-			}
-			Console.WriteLine("원하시는 채팅방 이름을 입력해주세요");
-			string ChatRoomName = Console.ReadLine();
+			int number = getInput();
 		b:
-			macro(number, str, ChatRoomName);
+			macro(number, str);
 			
 			Console.WriteLine("다시하시겠습니까?(예 - 0, 아니오 - 0이 아닌수)");
 			answer = Console.ReadLine();
@@ -45,7 +35,7 @@ namespace 카톡1
 				Environment.Exit(0);
 			}
 			
-			Console.WriteLine("이 설정 그대로 - 0 \n처음부터 다시 - 1 \n채팅방 이름만 바꾸기 - 2 \n도배할 말만 바꾸기 - 3 \n반복 횟수만 바꾸기 - 4");
+			Console.WriteLine("이 설정 그대로 - 0 \n처음부터 다시 - 1 \n도배할 말만 바꾸기 - 2 \n반복 횟수만 바꾸기 - 3");
 			answer = Console.ReadLine();
 		c:
 			if(answer == "0"){
@@ -55,25 +45,12 @@ namespace 카톡1
 				goto first;
 			}
 			else if(answer == "2"){
-				Console.WriteLine("원하시는 채팅방 이름을 입력해주세요");
-				ChatRoomName = Console.ReadLine();
-				goto b;
-			}
-			else if(answer == "3"){
 				Console.WriteLine("도배하시려는 말을 입력해주세요");
 				str = Console.ReadLine();
 				goto b;
 			}
-			else if(answer == "4"){
-				Console.WriteLine("얼마나 반복 하시겠습니까?");
-			d:
-				try{
-					number = int.Parse(Console.ReadLine());
-				}
-				catch{
-					Console.WriteLine("다시입력해주세요");
-					goto d;
-				}
+			else if(answer == "3"){
+				number = getInput();
 				goto b;
 			}
 			else{
@@ -81,11 +58,23 @@ namespace 카톡1
 				goto c;
 			}
 		}
-		
-		public static void macro(int num, string str, string ChatRoomName){
+		public static int getInput(){
+			Console.WriteLine("얼마나 반복하실겁니까?");
+			a:
+			int input = 0;
+			try{
+				input = int.Parse(Console.ReadLine());
+			}
+			catch{
+				Console.WriteLine("다시 입력해주세요.");
+				goto a;
+			}
+			return input;
+		}
+		public static void macro(int num, string str){
 			for(int c = 0; c < num; c++){
 				Console.WriteLine((c + 1) + "번 반복");
-				int i = FindWindow(null, ChatRoomName);
+				int i = FindWindow("#32770", null);
 				int a = FindWindowEx(i, 0, "RichEdit20W", null);
 				SendMessage(a, 0x000c, 0, str);
     			PostMessage(a, 0x0100, 0xD, 0x1C001);
